4be24f1349fe05fcc21237013943f1a01a7ca8af
TonyPurple <tony.vanoni@outlook.com>
2022-02-04 16:39:44 -0800

    adding items urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,13 @@ urlpatterns = [
   path('joshis/create/', views.JoshiCreate.as_view(), name='joshis_create'),
   path('joshis/<int:pk>/update/', views.JoshiUpdate.as_view(), name='joshis_update'),
   path('joshis/<int:pk>/delete/', views.JoshiDelete.as_view(), name='joshis_delete'),
+  # associate an item with a Joshi (M:M)
+  path('joshis/<int:joshi_id>/assoc_item/<int:item_id>/', views.assoc_item, name='assoc_item'),
+  # unassociate an item and Joshi
+  path('joshis/<int:joshi_id>/unassoc_item/<int:item_id>/', views.unassoc_item, name='unassoc_item'),
+  path('items/', views.ItemList.as_view(), name='items_index'),
+  path('items/<int:pk>/', views.ItemDetail.as_view(), name='items_detail'),
+  path('items/create/', views.ItemCreate.as_view(), name='items_create'),
+  path('items/<int:pk>/update/', views.ItemUpdate.as_view(), name='items_update'),
+  path('items/<int:pk>/delete/', views.ItemDelete.as_view(), name='items_delete'),
 ]
