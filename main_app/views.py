918dc56970513d24ca7e8c10fe05cef99c5895d0
TonyPurple <tony.vanoni@outlook.com>
2022-02-04 16:48:04 -0800

    adding item class views

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Joshi
+from django.views.generic import ListView, DetailView
+from .models import Joshi, Item
+
+class JoshiCreate(CreateView):
+  model = Joshi
+  fields = '__all__'
+
+class JoshiUpdate(UpdateView):
+  model = Joshi
+  fields = '__all__'
+
+class JoshiDelete(DeleteView):
+  model = Joshi
+  success_url = '/joshis/'
 
 # Add the following import
 from django.http import HttpResponse
@@ -17,16 +30,34 @@ def joshis_index(request):
 
 def joshis_detail(request, joshi_id):
   joshi = Joshi.objects.get(id=joshi_id)
-  return render(request, 'joshis/detail.html', { 'joshi': joshi } )
+    # Get the items the Joshi doesn't have
+  items_joshi_doesnt_have = Item.objects.exclude(id__in = joshi.items.all().values_list('id'))
+  return render(request, 'joshis/detail.html', { 'joshi': joshi,
+   # Add the items to be displayed
+    'items': items_joshi_doesnt_have } )
 
-class JoshiCreate(CreateView):
-  model = Joshi
+def assoc_item(request, joshi_id, item_id):
+  Joshi.objects.get(id=joshi_id).items.add(item_id)
+  return redirect('detail', joshi_id=joshi_id)
+
+def unassoc_item(request, joshi_id, item_id):
+  Joshi.objects.get(id=joshi_id).items.remove(item_id)
+  return redirect('detail', joshi_id=joshi_id)
+
+class ItemList(ListView):
+  model = Item
+
+class ItemDetail(DetailView):
+  model = Item
+
+class ItemCreate(CreateView):
+  model = Item
   fields = '__all__'
 
-class JoshiUpdate(UpdateView):
-  model = Joshi
+class ItemUpdate(UpdateView):
+  model = Item
   fields = '__all__'
 
-class JoshiDelete(DeleteView):
-  model = Joshi
-  success_url = '/joshis/'
+class ItemDelete(DeleteView):
+  model = Item
+  success_url = '/items/'
