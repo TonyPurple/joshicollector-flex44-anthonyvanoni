0114dfeb76b2cc2332468d23a6118226770d7189
TonyPurple <tony.vanoni@outlook.com>
2022-02-04 16:55:51 -0800

    adding Item model to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # import your models here
-from .models import Joshi
+from .models import Joshi, Item
 
 # Register your models here
 admin.site.register(Joshi)
+admin.site.register(Item)
